fb55eb95e232dddf9b49acf285ec8b95cbe92433
Таисия <taisiya2008ivanova@gmail.com>
2024-03-13 18:34:47 +0300

    второе занятие

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 document.getElementById('add-btn').onclick = addNewTask;
 
+document.addEventListener('keydown', check)
+function check(ev) {
+    if (ev.key==='Enter'){
+        addNewTask();
+    }
+    
+}
+
+document.getElementById('ListTask').onclick = markDone
+function markDone(ev){
+    const elem = ev.target
+    elem.style.textDecoration = 'line-through'
+}
+
+document.getElementById('ListTask').onmouseover = showActions
+function showActions(ev){
+    const btn = document.createElement('button')
+    btn.innerText='Удалить'
+    ev.target.append(btn)
+}
+
+document.getElementById('ListTask').onmouseout = hideActions
+function hideActions(ev){
+    document.getElementsByTagName
+    const btn = ev.target.getElementsByTagName('button').item(0)
+    btn.parentNode.removeChild(btn)
+}
+
+
 
 function addNewTask() {
     const taskNameElem = document.getElementById('taskinp');
-    const taskName = taskNameElem.value;
+    const taskName = taskNameElem.value.trim();
 
     if (taskName) {
         const listElem = document.getElementById('ListTask');
