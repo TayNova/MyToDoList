94704576ce718ead5580732cabab0c56f1ad6b4e
Таисия <taisiya2008ivanova@gmail.com>
2024-03-20 18:34:27 +0300

    new

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
+let maxNumberIdTask = 2
+
+
 document.getElementById('add-btn').onclick = addNewTask;
 
 document.addEventListener('keydown', check)
 function check(ev) {
-    if (ev.key==='Enter'){
+    if (ev.key === 'Enter') {
         addNewTask();
     }
-    
+
 }
 
-document.getElementById('ListTask').onclick = markDone
-function markDone(ev){
-    const elem = ev.target
-    elem.style.textDecoration = 'line-through'
+function markDone (elem) {
+    let labelElem = document.querySelector(`[for="${elem.id}"]`);
+    if (elem.checked){
+        labelElem.style.textDecoration = 'line-through';
+    } else {
+        labelElem.style.textDecoration=`none`;
+    }
 }
 
 document.getElementById('ListTask').onmouseover = showActions
-function showActions(ev){
+function showActions(ev) {
     const btn = document.createElement('button')
-    btn.innerText='Удалить'
+    btn.innerText = 'Удалить'
     ev.target.append(btn)
 }
 
 document.getElementById('ListTask').onmouseout = hideActions
-function hideActions(ev){
+function hideActions(ev) {
     document.getElementsByTagName
     const btn = ev.target.getElementsByTagName('button').item(0)
     btn.parentNode.removeChild(btn)
@@ -35,9 +41,17 @@ function addNewTask() {
     const taskName = taskNameElem.value.trim();
 
     if (taskName) {
+        maxNumberIdTask++;
+        
         const listElem = document.getElementById('ListTask');
+
+
         const newTaskElem = document.createElement('li');
-        newTaskElem.innerText = taskName;
+        newTaskElem.innerHTML = `
+        <div class="item-task">
+        <input class="chbox-to-change" id="chbox-task-${maxNumberIdTask}" name="chbox-task-${maxNumberIdTask}" type="checkbox" onchange="markDone(this)">
+        <label for="chbox-task-${maxNumberIdTask}">${taskName}</label>
+        </div>`;
 
         listElem.prepend(newTaskElem);
     }
